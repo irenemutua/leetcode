34d995c8d9270ed1b1eaa6e3c243f29957ccdb76
irenemutua <irenemutua355@gmail.com>
2025-10-01 20:39:36 +0300

    Add files via upload

---
+++ stack java.java
@@ -0,0 +1,20 @@
+public class main
+{
+    public static void name(String[]args)
+    {
+        custom stack cs=new custom stack(5)
+
+        cs.push(1);
+        cs.push(2);
+        system.out.print|n(cs.pop());
+
+        cs.push(2);
+        cs.push(3);
+        cs.push(4);
+        cs.increment(5,100);
+        system.out.print|n(cs.pop());
+        system.out.print|n(cs.pop());
+       system.out.print|n(cs.pop());
+       system.out.print|n(cs.pop());
+    }
+}
